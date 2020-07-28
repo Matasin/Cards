28106db24639b6202fa5ad162c93beb92dd2b1ea
Patryk <matasinqwer@gmail.com>
2020-07-28 22:57:26 +0200

    Updates

---
+++ precache-manifest.0abd4b3195e4ab7472e7c06f31949e96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c536e67b3302e53b04e8e5bc545ef356",
+    "url": "/Cards/index.html"
+  },
+  {
+    "revision": "d7f50cb09420984cc4fb",
+    "url": "/Cards/static/css/main.8d9e6c63.chunk.css"
+  },
+  {
+    "revision": "6dda28cc37b7d20f5987",
+    "url": "/Cards/static/js/2.c418994b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cards/static/js/2.c418994b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f50cb09420984cc4fb",
+    "url": "/Cards/static/js/main.4713ffd5.chunk.js"
+  },
+  {
+    "revision": "b5ff2be575df4dbee35a",
+    "url": "/Cards/static/js/runtime-main.2afa5da9.js"
+  }
+]);
